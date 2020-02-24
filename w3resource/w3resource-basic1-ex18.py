e46f4469ed3e4031f0eda358b2dfff0040cdc1f9
Vijay Kumar <vj_kumar20@hotmail.com>
2020-02-23 21:52:26 -0500

    Solution for exercise 18 of w3resource.

---
+++ w3resource/w3resource-basic1-ex18.py
@@ -0,0 +1,12 @@
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+number3 = int(input("Enter third number:"))
+
+def sumofthree(number1, number2, number3):
+    sum = number1 + number2 + number3
+    if number1 == number2 == number3:
+        sum = 3 * sum
+    return sum
+    
+print(sumofthree(number1, number2, number3))
+print(sumofthree(number1, number2, number3))
