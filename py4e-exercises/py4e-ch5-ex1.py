447b986a71c39b480347e9026e46fd7d5d310ae2
Vijay Kumar <vj_kumar20@hotmail.com>
2020-04-15 00:08:55 -0400

    Fulfilling all the requirements of the question

--- py4e-exercises/py4e-ch5-ex1.py
+++ py4e-exercises/py4e-ch5-ex1.py
@@ -1,13 +1,16 @@
 total = 0
 count = 0
+average = 0
 num = "start"
 
 while num != "done":
     try:
         num = input("Enter a number:")
-        total += int(num)    
+        total += int(num)
+        count += 1
     except:
         if( num != "done"):
             print("Invalid input")
             
-print(total)
+average = total/count
+print(total, count, average)
