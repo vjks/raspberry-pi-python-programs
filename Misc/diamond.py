db77c8ee638961d8d3559a928658d628b5a015ec
vjks <vjks@users.noreply.github.com>
2022-07-29 02:19:59 +0530

    Update diamond.py
    Removed commented code because it wasn't needed anymore.

--- Misc/diamond.py
+++ Misc/diamond.py
@@ -1,13 +1,11 @@
 input = int(input("Enter number of rows:" ))
 
 for x in range(1, input + 1):
-    #print(x)
     result = ("*" * x ) + ("*" * (x - 1))
     space = " " * ( ( input + 1 ) - x )
     print( space + result )
 
 for x in reversed(range(1, input)):
-    #print(x)
     result = ("*" * x ) + ("*" * (x - 1))
     space = " " * ( ( input + 1 ) - x )
     print( space + result )
