9900b4dc3e77aec3d424c03efb71d97c1534f274
vjks <vjks@users.noreply.github.com>
2022-09-13 18:58:49 -0400

    Create copies of a file with different filenames
    Connects to FTP. Gets filenames. Creates copies of files with those filenames.

---
+++ Misc/file_creator.py
@@ -0,0 +1,38 @@
+import shutil
+import ftplib
+import re
+import os
+
+def create_directory(link2LearningID):
+    directory = link2LearningID
+    parent_dir = "C:/Users/vj_ku/OneDrive/_Docs/Work/Wellnet/siemens_resources/"
+    path = os.path.join(parent_dir, directory)
+    os.mkdir(path)
+
+def get_data_from_ftp():
+    ftp = ftplib.FTP("ftp.cloud.scorm.com")
+    ftp.login("<Username>", "<Password>")
+    ftp.cwd('/LinkeLearningRealm/TYFLI9WJR3/courses/' + link2LearningID + '/2/story_content/external_files')
+    data = []
+    ftp.dir(data.append)
+    ftp.quit()
+    return data
+
+def create_files(data):
+    for line in data:
+        filename = re.findall('22:25\s.*(?:\.pdf|\.xls)', line)
+        if len(filename) > 0: 
+            filename = ''.join(filename)
+            filename = filename[6:]
+            print(filename)
+            original = r'C:\Users\vj_ku\OneDrive\_Docs\Work\Wellnet\siemens_resources\Siemens_-_generic_resource_substitution.pdf'
+            target = rf'C:\Users\vj_ku\OneDrive\_Docs\Work\Wellnet\siemens_resources\{link2LearningID}\\' + filename
+            shutil.copyfile(original, target)
+        else: 
+            print("Error. Check the content of the data variable")
+
+link2LearningID = 'Link2eLearning1157'
+create_directory(link2LearningID)
+data = get_data_from_ftp()
+create_files(data)
+
--- Misc/show_time.py
+++ Misc/show_time.py
@@ -11,12 +11,37 @@ def get_EST(unix_timestamp, ctd_seconds):
     date_time = datetime.datetime.fromtimestamp(second_country_unix_time).strftime("%H:%M:%S")
     print("The current Eastern Standard Time is: " + str(date_time))
 
+def is_daylight_Savings(unix_timestamp):
+    day_of_the_year = datetime.datetime.fromtimestamp(unix_timestamp).strftime("%j")
+    print("Today's day is:" + str(day_of_the_year))
+    date_numeral = datetime.datetime.today().weekday()
+    options = {
+        0 : "Monday",
+        1 : "Tuesday",
+        2 : "Wednesday",
+        3 : "Thursday",
+        4 : "Friday",
+        5 : "Saturday",
+        6 : "Sunday",
+    }
+    print(options.get(date_numeral))
+    if int(day_of_the_year) > 71 and int(day_of_the_year) < 311:
+        now = datetime.datetime.now()
+        today2am = now.replace(hour=2, minute=0, second=0, microsecond=0)
+        if now > today2am:
+            return True
+    else:
+        return False
+
+
 unix_timestamp = time.time()
 
 try:
-    current_time_difference = float(input("Enter current time difference as a decimal value:"))
     current_location = input("Enter the country you are in:")
-    ctd_seconds =  current_time_difference * 60 * 60
+    if is_daylight_Savings(unix_timestamp):
+        ctd_seconds =  9.5 * 60 * 60
+    else:
+        ctd_seconds = 10.5 * 60 * 60
     if current_location.lower() == "india":
         get_EST(unix_timestamp, ctd_seconds)
     elif current_location.lower() == "canada":
