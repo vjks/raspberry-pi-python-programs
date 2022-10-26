2907dbe10ef6faa4396640efb4d6cd5d538bb9ad
vjks <vjks@users.noreply.github.com>
2022-10-26 14:30:07 -0400

    Update file_creator.py

--- Misc/file_creator.py
+++ Misc/file_creator.py
@@ -12,7 +12,7 @@ def create_directory(link2LearningID):
 def get_data_from_ftp():
     ftp = ftplib.FTP("ftp.cloud.scorm.com")
     ftp.login("<Username>", "<Password>")
-    ftp.cwd('/LinkeLearningRealm/TYFLI9WJR3/courses/' + link2LearningID + '/2/story_content/external_files')
+    ftp.cwd('/LinkeLearningRealm/TYFLI9WJR3/courses/' + link2LearningID + '/2/story_content/WebObjects')
     data = []
     ftp.dir(data.append)
     ftp.quit()
@@ -21,18 +21,17 @@ def get_data_from_ftp():
 def create_files(data):
     for line in data:
         filename = re.findall('22:25\s.*(?:\.pdf|\.xls)', line)
-        if len(filename) > 0: 
+        if len(filename) > 0:
             filename = ''.join(filename)
             filename = filename[6:]
             print(filename)
             original = r'C:\Users\vj_ku\OneDrive\_Docs\Work\Wellnet\siemens_resources\Siemens_-_generic_resource_substitution.pdf'
             target = rf'C:\Users\vj_ku\OneDrive\_Docs\Work\Wellnet\siemens_resources\{link2LearningID}\\' + filename
             shutil.copyfile(original, target)
-        else: 
+        else:
             print("Error. Check the content of the data variable")
 
-link2LearningID = 'Link2eLearning1157'
+link2LearningID = 'Link2eLearning3284'
 create_directory(link2LearningID)
 data = get_data_from_ftp()
 create_files(data)
-
