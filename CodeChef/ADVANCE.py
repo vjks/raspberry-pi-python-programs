15721d062106f175e337c226cac57d474c0d43f3
vjks <vjks@users.noreply.github.com>
2023-05-14 01:46:07 +0530

    Revert "Create ADVANCE.py"
    This reverts commit af2172e7e8274f72d886e0a62f7a30b4bed53d3c.

--- CodeChef/ADVANCE.py
+++
@@ -1,8 +0,0 @@
-num_tests = int(input())
-
-for x in range(num_tests):
-    current_rating, problem_difficulty = map(int, input().split())
-    if current_rating <= problem_difficulty + 200:
-        print("YES")
-    else:
-        print("NO")
