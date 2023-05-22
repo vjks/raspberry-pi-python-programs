b24b473f0f9ef4904bbb69f041388f5d7b639648
vjks <vjks@users.noreply.github.com>
2023-05-22 13:46:47 +0530

    Revert "Create MANGOES.py"
    This reverts commit 0f85f625cc588e64ab3768ce347258401c8a5d20.

--- CodeChef/MANGOES.py
+++
@@ -1,5 +0,0 @@
-num_tests = int(input())
-
-for x in range(num_tests):
-    mango_weight, truck_weight, bridge_tolerance = map(int, input().split())
-    print((bridge_tolerance - truck_weight) // mango_weight)
