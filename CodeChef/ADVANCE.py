6601d6c3de96fef6c147a69742e9b3915c4e4b44
vjks <vjks@users.noreply.github.com>
2023-06-08 09:13:01 -0400

    Update ADVANCE.py
    Problem statement: https://www.codechef.com/problems/AORB
    
    Given the time taken to solve two problems and the points deducted per minute for not solving one, print the points obtained by using the most optimal strategy for solving the two problems.

--- CodeChef/ADVANCE.py
+++ CodeChef/ADVANCE.py
@@ -1,8 +1,12 @@
-num_tests = int(input())
+tests = int(input())
 
-for x in range(num_tests):
-    current_rating, problem_difficulty = map(int, input().split())
-    if problem_difficulty >= current_rating and problem_difficulty <= (current_rating + 200):
-        print("YES")
+for _ in range(tests):
+    a_time, b_time = map(int, input().split())
+    # The points deducted for the 2nd problem will be the sum of the time taken 
+    # for both problems.
+    solve_a_b = (500 - a_time * 2) + (1000 - (b_time + a_time) * 4)
+    solve_b_a = (1000 - b_time * 4) + (500 - (a_time + b_time) * 2)
+    if solve_a_b > solve_b_a:
+        print(solve_a_b)
     else:
-        print("NO")
+        print(solve_b_a)
