a365b90f97c5d37f8b1d54206691719c98b5a6f4
vjks <vjks@users.noreply.github.com>
2023-06-18 18:28:48 -0400

    Create roman_to_integer.py
    Problem statement: https://leetcode.com/problems/roman-to-integer/description
    
    Given a Roman numeral, print its decimal equivalent.

---
+++ CodeChef/roman_to_integer.py
@@ -0,0 +1,43 @@
+import sys
+assert sys.version_info >= (3, 10)
+
+class Solution:
+    def equivalent(self, symbol) -> int:
+        value = 0
+        match symbol:
+            case 'I':
+                value += 1
+            case 'V':
+                value += 5
+            case 'X':
+                value += 10
+            case 'L':
+                value += 50
+            case 'C':
+                value += 100
+            case 'D':
+                value += 500
+            case 'M':
+                value += 1000
+            case _:
+               value += 0
+        return value
+
+    
+    def romanToInt(self, s: str) -> int:
+        sum = 0
+        i = 0
+        while(i < len(s)):
+            if i + 1 < len(s):
+                current_element = self.equivalent(s[i])
+                next_element = self.equivalent(s[i + 1])
+                if(current_element < next_element):
+                    sum += (next_element - current_element)
+                    i += 2
+                else:
+                    sum += current_element
+                    i += 1
+            else:
+                sum += self.equivalent(s[i])
+                i += 1
+        return sum
