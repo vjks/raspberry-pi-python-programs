d0c3035f05d727e8ce043c6d07eecd2ce237a88b
vjks <vjks@users.noreply.github.com>
2023-06-20 09:39:49 -0400

    Create fizzbuzz-leetcode-easy.py
    Problem statement: https://leetcode.com/problems/fizz-buzz/description/
    
    Given a number, return an array that contains string equivalent of all the numbers leading up to n, or the words, "Fizz", "Buzz" or "FizzBuzz" based on whether the number is divisible by 3 and/or 5.

---
+++ CodeChef/fizzbuzz-leetcode-easy.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        array = []
+
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                array.append(str(i))
+                array[i - 1] = "FizzBuzz"
+            elif i % 3 == 0:
+                array.append(str(i))
+                array[i - 1] = "Fizz"
+            elif i % 5 == 0:
+                array.append(str(i))
+                array[i - 1] = "Buzz"
+            else:
+                array.append(str(i))
+        return array
